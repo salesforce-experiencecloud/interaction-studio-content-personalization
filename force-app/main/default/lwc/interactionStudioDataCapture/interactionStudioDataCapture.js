87c4517e89dc6bf7a49b57f86f3320467ba46e27
Sergey Agadzhanov <sagadzhanov@salesforce.com>
2021-07-26 14:37:29 +0200

    Converted indentation to spaces

--- force-app/main/default/lwc/interactionStudioDataCapture/interactionStudioDataCapture.js
+++ force-app/main/default/lwc/interactionStudioDataCapture/interactionStudioDataCapture.js
@@ -10,44 +10,44 @@ import userId from '@salesforce/user/Id'
 import { getRecord } from 'lightning/uiRecordApi';
 
 export default class InteractionStudioDataCapture extends LightningElement {
-	showPlaceholder = false;
+    showPlaceholder = false;
 
-	connectedCallback() {
-		/* Show placeholder content if we are in the Builder Context */
-		document.addEventListener('interactionstudio_onbuildercontextdetected', (e) => {
-			this.showPlaceholder = true;
-		});
+    connectedCallback() {
+        /* Show placeholder content if we are in the Builder Context */
+        document.addEventListener('interactionstudio_onbuildercontextdetected', (e) => {
+            this.showPlaceholder = true;
+        });
 
-		document.dispatchEvent(new CustomEvent('lwc_oncomponentready', {
-			bubbles: true,
-			composed: true			
-		}));
-	}
+        document.dispatchEvent(new CustomEvent('lwc_oncomponentready', {
+            bubbles: true,
+            composed: true			
+        }));
+    }
 
-	@wire(getRecord, { recordId: userId, layoutTypes: ['Full'], modes: ['View'] })
-	wiredRecord({ error, data }) {
-		if (!userId) {
-			this.initInteractionStudio(null);
-		}
-		else if (data) {
-			this.initInteractionStudio(data);
-			
-			document.addEventListener('evergage:onInit', () => {
-				setTimeout(() => {
-					this.initInteractionStudio(data);
-				}, 100);
-			})
-		}
-		else if (error) {
-			console.debug('wiredRecord error: ', error);
-		}
-	}
+    @wire(getRecord, { recordId: userId, layoutTypes: ['Full'], modes: ['View'] })
+    wiredRecord({ error, data }) {
+        if (!userId) {
+            this.initInteractionStudio(null);
+        }
+        else if (data) {
+            this.initInteractionStudio(data);
+            
+            document.addEventListener('evergage:onInit', () => {
+                setTimeout(() => {
+                    this.initInteractionStudio(data);
+                }, 100);
+            })
+        }
+        else if (error) {
+            console.debug('wiredRecord error: ', error);
+        }
+    }
 
-	initInteractionStudio(userData) {
-		document.dispatchEvent(new CustomEvent('lwc_onuserdataready', {
-			bubbles: true,
-			composed: true,
-			detail: { userData: userData }
-		}));
-	}
+    initInteractionStudio(userData) {
+        document.dispatchEvent(new CustomEvent('lwc_onuserdataready', {
+            bubbles: true,
+            composed: true,
+            detail: { userData: userData }
+        }));
+    }
 }
--- force-app/main/default/lwc/interactionStudioPersonalization/interactionStudioPersonalization.js
+++ force-app/main/default/lwc/interactionStudioPersonalization/interactionStudioPersonalization.js
@@ -7,84 +7,84 @@
 
 import { LightningElement, api } from 'lwc';
 export default class InteractionStudioPersonalization extends LightningElement {
-	@api contentZone = '';
+    @api contentZone = '';
 
-	get contentZoneSelector() {
-		return '#' + this.contentZone;
-	}
+    get contentZoneSelector() {
+        return '#' + this.contentZone;
+    }
 
-	showPlaceholder = false;
-	receivedContent = false;
+    showPlaceholder = false;
+    receivedContent = false;
 
-	get isContentZoneEmpty() {
-		return this.contentZone == '';
-	}
+    get isContentZoneEmpty() {
+        return this.contentZone == '';
+    }
 
-	connectedCallback() {
-		/* Show placeholder content if we are in the Builder Context */
-		document.addEventListener('interactionstudio_onbuildercontextdetected', (e) => {
-			this.showPlaceholder = !this.receivedContent;
-		});
-	}
+    connectedCallback() {
+        /* Show placeholder content if we are in the Builder Context */
+        document.addEventListener('interactionstudio_onbuildercontextdetected', (e) => {
+            this.showPlaceholder = !this.receivedContent;
+        });
+    }
 
-	renderedCallback() {
-		this.addContentListener();
-		this.dispatchComponentReadyEvent();
-	}
+    renderedCallback() {
+        this.addContentListener();
+        this.dispatchComponentReadyEvent();
+    }
 
-	addContentListener() {
-		document.addEventListener('interactionstudio_oncontentforcomponentready', (e) => {
-			if (e.detail && e.detail.selector == this.contentZoneSelector) {
-				/* Hide placeholder if content is received */
-				this.receivedContent = true;
-				this.showPlaceholder = false;
+    addContentListener() {
+        document.addEventListener('interactionstudio_oncontentforcomponentready', (e) => {
+            if (e.detail && e.detail.selector == this.contentZoneSelector) {
+                /* Hide placeholder if content is received */
+                this.receivedContent = true;
+                this.showPlaceholder = false;
 
-				/* Set real content of the LWC */
-				this.template.querySelector('div.interactionstudio_content').innerHTML = e.detail.html;
+                /* Set real content of the LWC */
+                this.template.querySelector('div.interactionstudio_content').innerHTML = e.detail.html;
 
-				/* Send Impression stat if needed */
-				if (e.detail.dontSendImpression !== true) {
-					this.sendStat('Impression', this.template.querySelector('[data-evg-experience-id]'));
-				}
+                /* Send Impression stat if needed */
+                if (e.detail.dontSendImpression !== true) {
+                    this.sendStat('Impression', this.template.querySelector('[data-evg-experience-id]'));
+                }
 
-				/* Send Clickthrough stat when clicked on anchor elements */
-				this.addExperienceClickListener();
-			}
-		});
-	}
+                /* Send Clickthrough stat when clicked on anchor elements */
+                this.addExperienceClickListener();
+            }
+        });
+    }
 
-	addExperienceClickListener() {
-		/* Capture any clicks and send campaign stats */
-		this.template.querySelector('div.interactionstudio_content a').addEventListener('click', (e) => {
-			this.sendStat('Clickthrough', e?.toElement?.closest('[data-evg-experience-id]'));
-		});
-	}
+    addExperienceClickListener() {
+        /* Capture any clicks and send campaign stats */
+        this.template.querySelector('div.interactionstudio_content a').addEventListener('click', (e) => {
+            this.sendStat('Clickthrough', e?.toElement?.closest('[data-evg-experience-id]'));
+        });
+    }
 
-	dispatchComponentReadyEvent() {
-		document.dispatchEvent(new CustomEvent('lwc_oncomponentready', {
-			bubbles: true,
-			composed: true,
-			detail: { contentZone: this.contentZone }
-		}));
-	}
+    dispatchComponentReadyEvent() {
+        document.dispatchEvent(new CustomEvent('lwc_oncomponentready', {
+            bubbles: true,
+            composed: true,
+            detail: { contentZone: this.contentZone }
+        }));
+    }
 
-	sendStat(stat, experienceElement) {
-		if (!experienceElement) return;
+    sendStat(stat, experienceElement) {
+        if (!experienceElement) return;
 
-		const payload = {
-			campaignStats: [
-				{
-					stat: stat,
-					experienceId: experienceElement.getAttribute('data-evg-experience-id'),
-					control: experienceElement.getAttribute('data-evg-user-group') === 'Control',
-				}
-			]
-		};
+        const payload = {
+            campaignStats: [
+                {
+                    stat: stat,
+                    experienceId: experienceElement.getAttribute('data-evg-experience-id'),
+                    control: experienceElement.getAttribute('data-evg-user-group') === 'Control',
+                }
+            ]
+        };
 
-		document.dispatchEvent(new CustomEvent('lwc_onstatsend', { 
-			bubbles: true,
-			composed: true,
-			detail: payload 
-		}));
-	}
+        document.dispatchEvent(new CustomEvent('lwc_onstatsend', { 
+            bubbles: true,
+            composed: true,
+            detail: payload 
+        }));
+    }
 }
